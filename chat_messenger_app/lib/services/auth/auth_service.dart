16b6babb77b4a9f412b2aba7ed520c0f045869bc
Kleyton Carv <kleyton_ky@hotmail.com>
2023-12-15 18:17:48 -0300

    add6

--- chat_messenger_app/lib/services/auth/auth_service.dart
+++ chat_messenger_app/lib/services/auth/auth_service.dart
@@ -1,9 +1,13 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/foundation.dart';
 
 class AuthService extends ChangeNotifier {
   //instance of auth
   final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+
+  // instance of firestore
+  final FirebaseFirestore _fireStore = FirebaseFirestore.instance;
 
   //sign user in
   Future<UserCredential> signInWithEmailandPassword(
@@ -15,6 +19,12 @@ class AuthService extends ChangeNotifier {
         email: email,
         password: password,
       );
+
+      // add a new document for the user in users collection if it doesn't already exists
+      _fireStore.collection('users').doc(userCredential.user!.uid).set({
+        'uid': userCredential.user!.uid,
+        'email': email,
+      }, SetOptions(merge: true));
 
       return userCredential;
     }
@@ -33,6 +43,12 @@ class AuthService extends ChangeNotifier {
         email: email,
         password: password,
       );
+
+      // after creating the user, create a new document for the user in the users collection
+      _fireStore.collection('users').doc(userCredential.user!.uid).set({
+        'uid': userCredential.user!.uid,
+        'email': email,
+      });
 
       return userCredential;
     } on FirebaseAuthException catch (e) {
