b309509fd9d935e58a9ddc03958f7801527d3358
Kleyton Carv <kleyton_ky@hotmail.com>
2023-12-16 01:19:31 -0300

    add 8

--- chat_messenger_app/lib/pages/chat_page.dart
+++ chat_messenger_app/lib/pages/chat_page.dart
@@ -1,5 +1,7 @@
+import 'package:chat_messenger_app/components/chat_bubble.dart';
 import 'package:chat_messenger_app/components/my_text_field.dart';
 import 'package:chat_messenger_app/services/auth/chat/chat_service.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
@@ -34,26 +36,77 @@ class _ChatPageState extends State<ChatPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text(widget.receiverUserEmail)),
-      body: Column(
-        children: [
-          // messages
-          Expanded(child: _buildMessageList(),
-          ),
+        appBar: AppBar(title: Text(widget.receiverUserEmail)),
+        body: Column(
+          children: [
+            // messages
+            Expanded(
+              child: _buildMessageList(),
+            ),
 
-          // user input
-          _buildMessageInput();
-        ],
-      )
+            // user input
+            _buildMessageInput(),
+          ],
+        ));
+  }
+
+  // build message list
+  Widget _buildMessageList() {
+    return StreamBuilder(
+      stream: _chatService.getMessages(
+          widget.receiverUserID, _firebaseAuth.currentUser!.uid),
+      builder: (context, snapshot) {
+        if (snapshot.hasError) {
+          return Text('Error${snapshot.error}');
+        }
+
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Text('Loading..');
+        }
+
+        return ListView(
+          children: snapshot.data!.docs
+              .map((document) => _buildMessageItem(document))
+              .toList(),
+        );
+      },
     );
   }
 
-  // build message list 
-
   // build message item
+  Widget _buildMessageItem(DocumentSnapshot document) {
+    Map<String, dynamic> data = document.data() as Map<String, dynamic>;
+
+    // align the messages to the right if the sender is the current user, otherwise to the left
+    var alignment = (data['senderId'] == _firebaseAuth.currentUser!.uid)
+        ? Alignment.centerRight
+        : Alignment.centerLeft;
+
+    return Container(
+      alignment: alignment,
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          crossAxisAlignment:
+              (data['senderId'] == _firebaseAuth.currentUser!.uid)
+                  ? CrossAxisAlignment.end
+                  : CrossAxisAlignment.start,
+          mainAxisAlignment:
+              (data['senderId'] == _firebaseAuth.currentUser!.uid)
+                  ? MainAxisAlignment.end
+                  : MainAxisAlignment.start,
+          children: [
+            Text(data['senderEmail']),
+            const SizedBox(height: 5),
+            ChatBubble(message: data['message']),
+          ],
+        ),
+      ),
+    );
+  }
 
   // build message input
-  Widget _buildMessageInput(){
+  Widget _buildMessageInput() {
     return Row(
       children: [
         //textfield
@@ -66,7 +119,13 @@ class _ChatPageState extends State<ChatPage> {
         ),
 
         // send button
-        
+        IconButton(
+          onPressed: sendMessage,
+          icon: const Icon(
+            Icons.arrow_upward,
+            size: 40,
+          ),
+        ),
       ],
     );
   }
--- chat_messenger_app/lib/pages/home_page.dart
+++ chat_messenger_app/lib/pages/home_page.dart
@@ -29,7 +29,7 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Home Page!!"),
+        title: const Text("Bate papo!"),
         actions: [
           //sign out button
           IconButton(
