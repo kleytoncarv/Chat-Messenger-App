cb2fcc9112afd82bf13dd981431af359726abbe5
Kleyton Carv <kleyton_ky@hotmail.com>
2023-12-16 00:24:54 -0300

    add 7

--- chat_messenger_app/lib/pages/chat_page.dart
+++ chat_messenger_app/lib/pages/chat_page.dart
@@ -1,3 +1,6 @@
+import 'package:chat_messenger_app/components/my_text_field.dart';
+import 'package:chat_messenger_app/services/auth/chat/chat_service.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class ChatPage extends StatefulWidget {
@@ -14,10 +17,57 @@ class ChatPage extends StatefulWidget {
 }
 
 class _ChatPageState extends State<ChatPage> {
+  final TextEditingController _messageController = TextEditingController();
+  final ChatService _chatService = ChatService();
+  final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+
+  void sendMessage() async {
+    //only send message if there is something to send
+    if (_messageController.text.isNotEmpty) {
+      await _chatService.sendMessage(
+          widget.receiverUserID, _messageController.text);
+      // clear the text controller after sending the message
+      _messageController.clear();
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text(widget.receiverUserEmail)),
+      body: Column(
+        children: [
+          // messages
+          Expanded(child: _buildMessageList(),
+          ),
+
+          // user input
+          _buildMessageInput();
+        ],
+      )
+    );
+  }
+
+  // build message list 
+
+  // build message item
+
+  // build message input
+  Widget _buildMessageInput(){
+    return Row(
+      children: [
+        //textfield
+        Expanded(
+          child: MyTextField(
+            controller: _messageController,
+            hintText: 'Enter message',
+            obscureText: false,
+          ),
+        ),
+
+        // send button
+        
+      ],
     );
   }
 }
